0c2a9531f0fd90454dfe973d69fa4df1dd363004
Vaxied <psgmarcelo@gmail.com>
2024-02-06 21:49:08 -0400

    Using React Props

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './TodoList.css'
+import React from "react";
+import "./TodoList.css";
 
 function TodoList(props) {
   return (
-    <section>
-      <ul>
-        {props.children}
-      </ul>
+    <section className="TodoList-container">
+      {props.error && props.onError()}
+      {props.loading && props.onLoading()}
+      {!props.loading && !props.searchedTodos.length && props.onEmptyTodos()}
+      <ul>{props.searchedTodos.map(props.render)}</ul>
     </section>
   );
 }
