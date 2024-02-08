9afb094943980ba848324e83f126ed0a700581f1
Vaxied <psgmarcelo@gmail.com>
2024-02-07 23:53:44 -0400

    introduction to High Order Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App/index.js";
+import "./index.css";
+
+function App(props) {
+  return (
+    <h1>
+      {props.greeting}, {props.name}
+    </h1>
+  );
+}
+
+function withGreeting(greeting) {
+  return function WrappedComponentWithGreeting(WrappedComponent) {
+    return function Component(props) {
+      return (
+        <>
+          <WrappedComponent {...props} greeting={greeting} />
+        </>
+      );
+    };
+  };
+}
+
+const AppWithGreeting = withGreeting("Hey")(App);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <AppWithGreeting name="Arnold" />,
+  // /* <App />,
+  document.getElementById("root")
 );
